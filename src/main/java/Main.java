614200b15cbadf0b12db0080409c6133341beb54
felipe <felipegaiacharly@gmail.com>
2016-01-31 17:37:23 -0600

    login activity example

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+/**
+ * Created by marconi on 28/01/16.
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import org.kairos.components.MaterialButton;
+import org.kairos.core.ActivityFactory;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root=new StackPane(); // Create root pane
+        stage.setScene(new Scene(root)); // Set the scene in the stage
+
+        // this object represent the stack  of activities  in your application
+        ActivityFactory factory=new ActivityFactory(stage);
+
+        // set the material design style in your application
+        stage.getScene().getStylesheets().add(MaterialButton.class.getResource("controls.css").toExternalForm());
+
+        factory.startActivity(MainActivity.class); // start the activity
+        stage.show();
+    }
+}
---
+++ src/main/java/MainActivity.java
@@ -0,0 +1,13 @@
+import org.kairos.core.Activity;
+
+/**
+ * Created by marconi on 28/01/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setContentView(getClass().getResource("main_activity.fxml"));
+    }
+}
